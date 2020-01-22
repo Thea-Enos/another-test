2d17e61fb8aba0c3745a5a8b0c6255364d7b02fb
Thea <thea@climbcred.it>
2020-01-21 21:57:44 -0500

    Updates

---
+++ precache-manifest.ebd01c6b09ef9fbbc7f06ed0e18aaff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3631c74b51d86570418804ecce1e312",
+    "url": "/another-test/index.html"
+  },
+  {
+    "revision": "77df62c08194cc8bd9a6",
+    "url": "/another-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "32d257b91b01bad39656",
+    "url": "/another-test/static/js/2.413cde84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/another-test/static/js/2.413cde84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77df62c08194cc8bd9a6",
+    "url": "/another-test/static/js/main.4b19767f.chunk.js"
+  },
+  {
+    "revision": "0a6406820f6e3d778f58",
+    "url": "/another-test/static/js/runtime-main.bb113ccc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/another-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
